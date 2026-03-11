3099ba0713e0ec308573dd7bf1c93ba710f4de83
curlango-dev <curlango@uabc.edu.mx>
2026-03-11 16:04:44 -0700

    Todos los archivos que se requieren para poder descargar el proyecto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module uabc.curlango.demodominoxxx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.desktop;
+
+
+    opens uabc.curlango.demodominoxxx to javafx.fxml;
+    exports uabc.curlango.demodominoxxx;
+}
